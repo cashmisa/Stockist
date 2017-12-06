2ac12168a2712f1ac75d5c276ddec2cb9b94214b
Nhat Ton <minhnhattonthat@gmail.com>
2017-12-06 14:39:31 +0800

    Initial commit

---
+++ src/main/java/com/sa45team7/stockist/config/WebConfig.java
@@ -0,0 +1,50 @@
+package com.sa45team7.stockist.config;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+@Configuration
+public class WebConfig extends WebMvcConfigurerAdapter{
+	
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+	    registry.addInterceptor(localeChangeInterceptor());
+	}
+	
+	@Bean
+	public LocaleChangeInterceptor localeChangeInterceptor() {
+	    LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+	    lci.setParamName("lang");
+	    return lci;
+	}
+	
+	@Bean
+	public LocaleResolver localeResolver() {
+
+		CookieLocaleResolver cookieLocaleResolver = new CookieLocaleResolver();
+		cookieLocaleResolver.setDefaultLocale(StringUtils
+				.parseLocaleString("en"));
+		return cookieLocaleResolver;
+	}
+	
+	@Bean
+	public MessageSource messageSource() {
+		ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+		messageSource.setBasename("i18/messages");
+		messageSource.setUseCodeAsDefaultMessage(true);
+		messageSource.setDefaultEncoding("UTF-8");
+		// # -1 : never reload, 0 always reload
+		messageSource.setCacheSeconds(0);
+		return messageSource;
+	}
+	
+}
---
+++ src/main/webapp/WEB-INF/views/login.jsp
@@ -0,0 +1,31 @@
+<%@ taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
+<%@ taglib uri="http://www.springframework.org/tags" prefix="spring"%>
+
+<html>
+<spring:url value="/css/simple.css" var="style" />
+<link rel="STYLESHEET" type="text/css" href="${style}" />
+<form:form method="POST" commandName="login">
+	<table class="framed">
+		<tr>
+			<td><spring:message code="fieldLabel.username" /></td>
+			<td colspan="3"><input type="text" name="username" size="40" /></td>
+		</tr>
+		<tr>
+			<td><spring:message code="fieldLabel.password" /></td>
+			<td colspan="3"><input type="password" name="password" size="40" /></td>
+		</tr>
+		<tr>
+			<td colspan="4">&nbsp;</td>
+		</tr>
+		<tr>
+			<td>&nbsp;</td>
+<%-- 			<td><form:button name="submit" type="submit" value="s">
+					<img src="/image/button_login.gif" alt="" align="middle">
+				</form:button></td>
+			<td><form:button name="clear" type="reset" value="r">
+					<img src="/image/button_clear.gif" alt="" align="middle">
+				</form:button></td> --%>
+		</tr>
+	</table>
+</form:form>
+</html>
