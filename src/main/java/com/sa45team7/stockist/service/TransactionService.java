15722a6d0c1a43931d24f8e213c6237ad19a3ddf
mayzinko <mayzinko@172.17.249.86>
2017-12-06 18:59:45 +0800

    transactionservice

---
+++ src/main/java/com/sa45team7/stockist/service/TransactionService.java
@@ -0,0 +1,21 @@
+package com.sa45team7.stockist.service;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+import com.sa45team7.stockist.model.Transaction;
+
+
+
+public interface TransactionService
+{
+	
+	ArrayList<Transaction> findAllTransaction();
+	
+	Transaction findTransactionById(Integer tranId);
+	
+	ArrayList<Transaction> findTransactionByPartNo(Integer partNo);
+	
+	ArrayList<Transaction> findTransactionByDate(Date startDate,Date endDate);
+
+}
