156cffbcb467b91ee5fc97cd4704a6f2a53461da
ongxinying <ongxinying@172.17.253.38>
2017-12-07 13:10:46 +0800

    Annotation for request mapping (controllers)

--- src/main/java/com/sa45team7/stockist/controller/AdminReportController.java
+++ src/main/java/com/sa45team7/stockist/controller/AdminReportController.java
@@ -1,5 +1,10 @@
 package com.sa45team7.stockist.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/admin/report")
+@Controller
 public class AdminReportController {
 
 }
